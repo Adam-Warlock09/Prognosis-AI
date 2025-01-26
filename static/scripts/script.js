590efab9bf3f32295c01a23383779125247fc080
Adam-Warlock09 <aricm1809@gmail.com>
2025-01-26 15:05:56 +0530

    added welcome pages

---
+++ static/scripts/script.js
@@ -0,0 +1,44 @@
+let imgContainer = document.getElementById("img")
+let mainContainer = document.getElementsByClassName("main-container")[0]
+let headText = document.getElementsByClassName("head-text")[0]
+let bodyContainer = document.getElementsByClassName("body-container")[0]
+let textContainer = document.getElementsByClassName("text-container")[0]
+let startBtn = document.getElementById("start-btn")
+let formBackground = document.getElementsByClassName("form-background")[0]
+
+imgContainer.addEventListener("click", () => {
+
+    imgContainer.classList.add("fade-out")
+
+    const imgTimeout = setTimeout(() => {
+
+        imgContainer.classList.add("no-display")
+        mainContainer.classList.toggle("no-display")
+
+        headText.style.animation = "movement 1s";
+
+        bodyContainer.style.animation = "fade-in 2s";
+
+        clearTimeout(imgTimeout)
+
+    }, 1000)
+
+})
+
+startBtn.addEventListener("click", ()=> {
+
+    textContainer.classList.add("fade-out")
+
+    const startTimeout = setTimeout(() => {
+
+        textContainer.classList.add("no-display")
+
+        formBackground.classList.toggle("no-display")
+
+        formBackground.style.animation = "fade-in 2s";
+
+        clearTimeout(startTimeout)
+
+    }, 1000)
+
+})
