d3950387cdd5cd0a69c79984ca3a2cb1c84ea792
Adam-Warlock09 <aricm1809@gmail.com>
2025-01-28 04:10:44 +0530

    almost done

--- app.py
+++ app.py
@@ -1,9 +1,74 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('./index.html')
+
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        formData = request.form
+        keysDict = {
+            "age" : "Number",
+            "gender" : "String",
+            "height" : "Number",
+            "weight" : "Number",
+            "physical-activity" : "Number",
+            "sleep-quality" : "Number",
+            "diet-quality" : "Number",
+            "cholestrol" : "Number",
+            "haemo" : "Number",
+            "bp" : "Number",
+            "heart" : "Number",
+            "ecg" : "CheckBox",
+            "ccp" : "CheckBox",
+            "anxiety" : "CheckBox",
+            "sugar" : "Number",
+            "insulin" : "Number",
+            "smoke" : "CheckBox",
+            "thick-feet" : "CheckBox",
+            "fam-dia" : "CheckBox",
+            "alcohol" : "CheckBox",
+            "firstPeriod" : "Number",
+            "pregnancies" : "Number",
+            "firstBaby" : "Number",
+            "menopause" : "CheckBox",
+            "fam-bc" : "CheckBox",
+            "chronicCough" : "CheckBox",
+            "allergies" : "CheckBox",
+            "yellowFingers" : "CheckBox",
+            "shortBreath" : "CheckBox",
+            "swallowing" : "CheckBox",
+            "fam-asthma" : "CheckBox",
+            "pollution" : "Number"
+        }
+
+        inputDict = {}
+
+        for i in keysDict:
+            if(keysDict[i] == "Number"):
+                inputDict[i] = formData.get(i)
+            elif(keysDict[i] == "String"):
+                inputDict[i] = formData.get(i).strip()
+            elif(keysDict[i] == "CheckBox"):
+                if(formData.getlist(i)):
+                    inputDict[i] = 1
+                else:
+                    inputDict[i] = 0
+        
+        # for i in inputDict:
+        #     if i == "gender":
+        #         inputDict[i] = (inputDict[i].lower() == "male")
+        #     elif i == "ccp":
+        #         if inputDict[i] == 1:
+        #             inputDict[i] = 2
+        #     elif i == "bp":
+
+
+        return render_template('./result.html', s = str(inputDict))
+    
     return render_template('./index.html')
 
 if __name__ == "__main__":
---
+++ static/scripts/form.js
@@ -0,0 +1,20 @@
+let x = document.getElementsByClassName('submit')
+let l = x.length
+let form = document.getElementById('details-form')
+
+for(i = 0; i<l; i++){
+    x[i].onclick = ((num, form) => {
+        return () => {
+
+                if(num < l-1){
+                    document.getElementsByClassName('form-container')[num].classList.add('no-display')
+                    document.getElementsByClassName('form-container')[num + 1].classList.remove('display')
+                }else{
+                    console.log("hello")
+                    // form.submit()
+                    console.log("hello submitted")
+                }
+
+        }
+    })(i,form)
+}
