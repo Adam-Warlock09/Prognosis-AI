7613a57df50e4209e67683bcf901a132774d99e0
Adam-Warlock09 <aricm1809@gmail.com>
2025-01-28 20:45:39 +0530

    almost done

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, redirect, request
+import pickle
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -49,7 +52,13 @@ def result():
 
         for i in keysDict:
             if(keysDict[i] == "Number"):
-                inputDict[i] = formData.get(i)
+                if(formData.get(i)):
+                    if(float(formData.get(i)) == round(float(formData.get(i)))):
+                        inputDict[i] = int(formData.get(i))
+                    else:
+                        inputDict[i] = float(formData.get(i))
+                else:
+                    inputDict[i] = 0
             elif(keysDict[i] == "String"):
                 inputDict[i] = formData.get(i).strip()
             elif(keysDict[i] == "CheckBox"):
@@ -70,12 +79,14 @@ def result():
             val = 4
         
         inputDict["bmi_breast"] = val
+
+        temp = {}
         
         for i in inputDict:
             if i == "gender":
-                inputDict[i] = (inputDict[i].lower() == "male")
+                inputDict[i] = int(inputDict[i].lower() == "male")
             elif i == "ccp":
-                inputDict["ccp_lung"] = inputDict[i] + 1
+                temp["ccp_lung"] = inputDict[i] + 1
                 if inputDict[i] == 1:
                     inputDict[i] = 2
             elif i == "smoke":
@@ -94,6 +105,16 @@ def result():
                 inputDict[i] += 1
             elif i == "swallowing":
                 inputDict[i] += 1
+            elif i == "sugar":
+                if(inputDict[i] > 120):
+                    inputDict[i] = 1
+                else:
+                    inputDict[i] = 0
+            elif i == "thick-feet":
+                if inputDict[i] == 1:
+                    inputDict[i] = 40
+                else:
+                    inputDict[i] = 20
             elif i == "age":
                 x = inputDict[i]
                 if(x>0 and x<=29):
@@ -122,7 +143,7 @@ def result():
                     y = 12
                 elif(x>=85):
                     y = 13
-                inputDict["age_breast"] = y
+                temp["age_breast"] = y
             elif i == "firstPeriod":
                 if inputDict[i] >= 14:
                     y = 0
@@ -146,9 +167,103 @@ def result():
             elif i == "menopause":
                 inputDict[i] += 1
 
-        return render_template('./result.html', s = str(inputDict))
+        inputDict.update(temp)
+
+        # List of features of each model
+        # Heart disease feature list--------------
+        heart_columns = ['age', 'sex', 'cp', 'trestbps', 'chol','fbs','restecg','thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
+        heart_default = [0, 1.07, 1, 0, 2]
+        heart_test_input = [inputDict["age"], inputDict["gender"], inputDict["ccp"], inputDict["bp"], inputDict["cholestrol"], inputDict["sugar"], inputDict["ecg"], inputDict["heart"]]
+        heart_test = heart_test_input + heart_default
+        heart_dict = dict(zip(heart_columns,heart_test))
+        heart_input = pd.DataFrame([heart_dict])
+
+        # -------------------------------------
+
+        diabetes_columns = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+        diabetes_test_input = [inputDict["pregnancies"], inputDict["sugar"], inputDict["bp"], inputDict["thick-feet"], inputDict["insulin"], inputDict["bmi"], inputDict["fam-dia"], inputDict["age"]]
+        diabetes_test = diabetes_test_input
+        diabetes_dict = dict(zip(diabetes_columns,diabetes_test))
+        diabetes_input = pd.DataFrame([diabetes_dict])
+        
+        # -------------------------------------
+
+        lung_cancer_columns = ['GENDER', 'AGE', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY', 'ALLERGY ', 'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH', 'SWALLOWING DIFFICULTY', 'CHEST PAIN', 'CHRONIC DISEASE', 'WHEEZING']
+        lung_cancer_default = [2,1]
+        lung_cancer_test_input = [inputDict["gender"], inputDict["age"], inputDict["smoke"], inputDict["yellowFingers"], inputDict["anxiety"], inputDict["allergies"], inputDict["alcohol"], inputDict["chronicCough"], inputDict["shortBreath"], inputDict["swallowing"], inputDict["ccp"]]
+        lung_cancer_test = lung_cancer_test_input + lung_cancer_default
+        lung_cancer_dict = dict(zip(lung_cancer_columns,lung_cancer_test))
+        lung_cancer_input = pd.DataFrame([lung_cancer_dict])
+
+        # -------------------------------------
+
+        anaemia_columns = ['Sex', 'Hb']
+        anaemia_test_input = [inputDict["gender"], inputDict["haemo"]]
+        anaemia_test = anaemia_test_input
+        anaemia_dict = dict(zip(anaemia_columns,anaemia_test))
+        anaemia_input = pd.DataFrame([anaemia_dict])
+
+        # -------------------------------------
+
+        asthma_columns = ['Age', 'Gender', 'BMI', 'Smoking', 'PhysicalActivity', 'DietQuality', 'SleepQuality', 'PollutionExposure', 'PollenExposure', 'DustExposure', 'PetAllergy', 'FamilyHistoryAsthma', 'HistoryOfAllergies','ShortnessOfBreath','Coughing', 'Eczema','HayFever','GastroesophagealReflux','LungFunctionFEV1','LungFunctionFVC','Wheezing', 'ChestTightness','NighttimeSymptoms','ExerciseInduced']
+        asthma_default = [0, 0, 0, 2.55, 3.74, 0, 1, 0, 1]
+        asthma_test_input = [inputDict["age"], inputDict["gender"], inputDict["bmi"], inputDict["smoke"] - 1, inputDict["physical-activity"], inputDict["diet-quality"], inputDict["sleep-quality"], inputDict["pollution"],  inputDict["pollution"], inputDict["pollution"], inputDict["pollution"], inputDict["allergies"] - 1, inputDict["fam-asthma"], inputDict["allergies"] - 1, inputDict["shortBreath"] - 1, inputDict["chronicCough"] - 1]
+        asthma_test = asthma_test_input + asthma_default
+        asthma_dict = dict(zip(asthma_columns,asthma_test))
+        asthma_input = pd.DataFrame([asthma_dict])
+
+        # -------------------------------------
+
+        breast_cancer_columns = ['Age_group', 'family_history', 'age_menarche' ,'age_first_birth', 'menopaus','bmi_group', 'Race' , 'current_hrt','biophx']
+        breast_cancer_default = [1, 0, 0]
+        breast_cancer_test_input = [inputDict["age_breast"], inputDict["fam-bc"], inputDict["firstPeriod"], inputDict["firstBaby"], inputDict["menopause"], inputDict["bmi_breast"]]
+        breast_cancer_test = breast_cancer_test_input + breast_cancer_default
+        breast_cancer_dict = dict(zip(breast_cancer_columns,breast_cancer_test))
+        breast_cancer_input = pd.DataFrame([breast_cancer_dict])
+
+        # --------------------------------------------------------
+
+        with open('ML_Models/anaemia.pkl', 'rb') as file:
+            anaemia_model = pickle.load(file)
+
+        with open('ML_Models/asthma.pkl', 'rb') as file:
+            asthma_model = pickle.load(file)
+
+        with open('ML_Models/breast.pkl', 'rb') as file:
+            breast_model = pickle.load(file)
+
+        with open('ML_Models/diabetes.pkl', 'rb') as file:
+            diabetes_model = pickle.load(file)
+
+        with open('ML_Models/heart.pkl', 'rb') as file:
+            heart_model = pickle.load(file)
+
+        with open('ML_Models/lung.pkl', 'rb') as file:
+            lung_model = pickle.load(file)
+
+        output = { "Anemia" : (anaemia_model.predict(anaemia_input)[0]), "Asthma" : (asthma_model.predict(asthma_input)[0]), "Breast Cancer" : (breast_model.predict(breast_cancer_input)[0]), "Diabetes" : (diabetes_model.predict(diabetes_input)[0]), "Heart Disease" : (heart_model.predict(heart_input)[0]), "Lung Cancer" : (lung_model.predict(lung_cancer_input)[0])}
+        
+        data = {"BMI" : round(inputDict["bmi"], 1)}
+        
+        for i in output:
+            number = output[i]
+            if(number < 0.6):
+                output[i] = "Low Risk"
+            elif(number >= 0.6 and number < 0.80):
+                output[i] = "Medium Risk"
+            elif(number >= 0.8 and number <=1):
+                output[i] = "High Risk"
+            else:
+                output[i] = "Sorry!"
+
+            data[i] = output[i]
+
+        if(inputDict["gender"] == 1):
+            del data["Breast Cancer"]
+
+        return render_template('./result.html', data = data)
     
-    return render_template('./index.html')
+    return redirect('/')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
--- static/scripts/form.js
+++ static/scripts/form.js
@@ -5,10 +5,14 @@ let form = document.getElementById('details-form')
 for(i = 0; i<l; i++){
     x[i].onclick = ((num, form) => {
         return () => {
-
-                if(num < l-1){
-                    document.getElementsByClassName('form-container')[num].classList.add('no-display')
-                    if(num == 2)
+            
+            let gender = document.getElementById('gender').value
+            
+            if(num < l-1){
+                document.getElementsByClassName('form-container')[num].classList.add('no-display')
+                if(num == 2 && gender.toLowerCase() == "male"){
+                        num += 1
+                    }
                     document.getElementsByClassName('form-container')[num + 1].classList.remove('display')
                 }else{
                     console.log("hello")
