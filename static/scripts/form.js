c34e727ab03133de150559cf8c1177d64c30eca7
Adam-Warlock09 <aricm1809@gmail.com>
2025-01-30 03:21:01 +0530

    changed dummy text

--- static/scripts/form.js
+++ static/scripts/form.js
@@ -2,6 +2,34 @@ let x = document.getElementsByClassName('submit')
 let l = x.length
 let form = document.getElementById('details-form')
 
+function validation(i) {
+
+    let inputList = document.getElementsByClassName(`input${i}`)
+
+    for(j = 0; j<inputList.length; j++){
+
+        inputList[j].setCustomValidity('');
+        if(!inputList[j].checkValidity()){
+            inputList[j].reportValidity();
+            return false
+        }
+
+        if(i == 1){
+
+            if(inputList[j].name == "gender"){
+                if(!["male","female"].includes(String(inputList[j].value).trim().toLowerCase())){
+                    inputList[j].setCustomValidity(`Enter either "Male" or "Female"`)
+                    inputList[j].reportValidity()
+                    return false
+                }
+            }
+
+        }
+
+    }
+
+}
+
 for(i = 0; i<l; i++){
     x[i].onclick = ((num, form) => {
         return () => {
@@ -11,9 +39,14 @@ for(i = 0; i<l; i++){
             if(num < l-1){
                 document.getElementsByClassName('form-container')[num].classList.add('no-display')
                 if(num == 2 && gender.toLowerCase() == "male"){
-                        num += 1
-                    }
-                    document.getElementsByClassName('form-container')[num + 1].classList.remove('display')
+                    num += 1
+                }else if(num == 2 && gender.toLowerCase() == "female"){
+                    let female_num_inputs = document.querySelectorAll('#form-container4 input[type=number]');
+                    female_num_inputs.forEach(input=> {
+                        input.required = true;
+                    })
+                }
+                document.getElementsByClassName('form-container')[num + 1].classList.remove('display')
                 }else{
                     console.log("hello")
                     // form.submit()
